b78f5f825d3f41aed4de36d42e9783ac499c7f5c
Jimin Huh <jiminhuh95@gmail.com>
2020-01-19 13:42:13 -0600

    added random number generator code as well

--- src/random_number_generator.java
+++ src/random_number_generator.java
@@ -1,18 +1,24 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class random_number_generator {
     public static void main(String[] args) {
 
-        int[] randomArray = {};
-
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter the maximum Number.");
 
         String maxNum = myObj.nextLine();
-        int MaxNumInt = Integer.parseInt(maxNum);
+        int maxNumInt = Integer.parseInt(maxNum);
+        int [] randomArray = new int[maxNumInt];
 
-        for (int num: randomArray) {
-            
+        for (int i = 0; i < maxNumInt; i++) {
+            randomArray[i] = i + 1;
         }
+
+        int randomNumber = new Random().nextInt(randomArray.length);
+        randomNumber = randomArray[randomNumber];
+
+        System.out.println("Your random number is : " + randomNumber);
+
     }
 }
